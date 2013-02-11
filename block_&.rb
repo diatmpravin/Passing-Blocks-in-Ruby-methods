891d7ca15a76d8d2d03ed48f1cb05acbb7513c72
Pravin  Mishra <diatm.pravin.it.07.27@gmail.com>
2013-02-11 23:31:45 +0530

    update code

--- block_&.rb
+++ block_&.rb
@@ -1,23 +1,23 @@
 
-# def test
-#    yield 5,10
-#    puts "You are in the method test"
-#    yield 100,200
-# end
+def test
+   yield 5,10
+   puts "You are in the method test"
+   yield 100,200
+end
 
-# def test
-#    yield 
-#    puts "You are in the method test"
-#    yield 
-# end
+def test
+   yield 
+   puts "You are in the method test"
+   yield 
+end
 
 
-# def test(b=Proc.new)
-#    # p b	
-#    b.call(10,20)
-#    puts "You are in the method test"
-#    yield 100,200
-# end
+def test(b=Proc.new)
+   p b   
+   b.call(10,20)
+   puts "You are in the method test"
+   yield 100,200
+end
 
 #The & calls 'to_proc' on the object, and passes it as a block to the method
 #In Rails, to_proc is implemented on Symbol, so that these two calls are equivalent:
